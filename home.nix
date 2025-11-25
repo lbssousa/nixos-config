a3f04f385510e0b8f922a5df57dda7ad3d76e429
Laércio de Sousa <laercio@sivali.sousa.nom.br>
2025-11-25 09:39:33 -0300

    Pequenos ajustes no módulo home.nix.

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "laercio";
-  home.homeDirectory = "/var/home/laercio";
+  home.homeDirectory = "/home/laercio";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   # Set git config
   programs.git = {
@@ -22,7 +22,7 @@
     userEmail = "laercio@sivali.sousa.nom.br";
     signing = {
       key = "5AB657FF72C72F35";
-      signByDefault = false;
+      signByDefault = true;
     };
     extraConfig = {
       init = {
@@ -36,8 +36,6 @@
       };
     };
   };
-
-  services.gpg-agent.pinentryPackage = pkgs.pinentry-gnome3;
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
